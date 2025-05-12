10bef9d0416225ca5653ea78e050f2a15ffd0d11
bom0320 <s24040@gsm.hs.kr>
2025-05-12 21:12:04 +0900

    chore: init commit

---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <>
+            <div>df</div>
+        </>
+    );
+}
+
+export default Home;
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <div>
+            adasfads
+        </div>
+    )
+}
+
+export default Page;
